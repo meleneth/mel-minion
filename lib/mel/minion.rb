0cc95aa337e102ce774a543e31ff0355ad213d38
Meleneth <meleneth@rinzler>
2023-02-13 20:34:34 -0800

    Initial sketches of functionality

---
+++ lib/mel/minion.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "minion/version"
+
+module Mel
+  module Minion
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/mel/minion/enable_rails_uuid_primary_keys.rb
@@ -0,0 +1,58 @@
+require 'securerandom'
+
+class EnableRailsUUIDPrimaryKeys
+  def self.run
+    if !File.exist?("Gemfile")
+      puts "This script must be run from the root directory of a Ruby on Rails application."
+      exit
+    end
+
+    create_migration("RailsEnablePgCrypto")
+    generate_initializer
+
+    puts "Migration and initializer files created successfully!"
+  end
+
+  private
+
+  # Get the timestamp in the format YYYYMMDDHHMMSS
+  def self.timestamp
+    Time.now.strftime("%Y%m%d%H%M%S")
+  end
+
+  # Generate the filename for the migration
+  def self.generate_filename(prefix)
+    "#{timestamp}_#{prefix}_#{SecureRandom.hex(4)}"
+  end
+
+  # Create a new migration file with the given name
+  def self.create_migration(name)
+    filename = "#{generate_filename(name)}_enable_pgcrypto.rb"
+    File.open("db/migrate/#{filename}", "w") do |file|
+      file.write <<-MIGRATION
+class #{name} < ActiveRecord::Migration[6.0]
+  def change
+    enable_extension 'pgcrypto'
+  end
+end
+      MIGRATION
+    end
+  end
+
+  # Generate the initializer to configure ActiveRecord to use UUID columns by default
+  def self.generate_initializer
+    initializer_file = "config/initializers/active_record_uuid.rb"
+    if !File.exist?(initializer_file)
+      File.open(initializer_file, "w") do |file|
+        file.write <<-INITIALIZER
+Rails.application.config.active_record.generators do |g|
+  g.orm :active_record, primary_key_type: :uuid
+end
+        INITIALIZER
+      end
+    end
+  end
+end
+
+EnableRailsUUIDPrimaryKeys.run
+
